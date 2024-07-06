6f95bdfb63e9dbb924268f76068239ca75da47d2
NodeNubie <144647694+NodeNubie@users.noreply.github.com>
2024-07-06 10:26:56 +0700

    Add files via upload

---
+++ data/config.py
@@ -0,0 +1,14 @@
+# api id, hash
+API_ID = 
+API_HASH = ''
+
+# delay between connections to accounts (the more accounts, the longer the delay)
+ACC_DELAY = [5, 15]
+
+# point per game
+POINT = [140, 160]
+
+# session folder (do not change)
+WORKDIR = "sessions/"
+
+
